69c064e4a8b344ecaa7d728671a9ffd885696b1f
fabs <dannybot77@gmail.com>
2021-08-02 17:18:42 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import profileImage from './img/image1.jpg';
+import profileImage from './img/image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>my react app</h1>
-      <img scr={profileImage} alt="profile-image"/>     
+      <img src={profileImage} alt="profile-image"/>     
     </div>
   );
 }
